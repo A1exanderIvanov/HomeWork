5121e6f5826aca823e658e735ca47d4053e2e680
A1exanderIvanov <sane2004@yandex.ru>
2021-10-04 21:55:24 +0300

    HomeWork

---
+++ HomeWork_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	short int a = 15;
+	int b = 365;
+	long long c = 14615358;
+	char d = 'x';
+	bool e = true;
+	float f = 1.41f;
+	double g = 18.698;
+
+
+
+	//char Sign_X = 'X';
+	//char Sign_O = 'O';
+
+	enum Game{Sign_X, Sign_O};
+
+	// согласно таблице ASCII символов пробел имеет код 32 
+	char playing_field[3][3] = {
+		{space,space,space},
+		{space,space,space},
+		{space,space,space}
+	};
+	std::cout << playing_field << endl;
+	
+}
---
+++ HomeWork_3_part_2.cpp
@@ -0,0 +1,5 @@
+
+extern const int a = 19;
+extern const int b = 23;
+extern const int c = 5;
+extern const int d = 8;
